e1ed3bf72788ce130e2e16e01b0f9e14b47d8ad7
phongidoit <93896974+phongidoit@users.noreply.github.com>
2023-03-14 18:03:31 +0700

    File code có sẵn trong slide

---
+++ Code.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n<0:
+        return ':D'
+    if n==0:
+        return 1
+    return factorial(n-1)*n
+
+def fibonacci(n):
+    if n <=1:
+        return 1
+    return fibonacci(n-1) + fibnacci(n-2)
+
+print(factorial(3))
+
+
